385a15846bdf71320b501830d8c45c748cffb4a6
s0r <a.mendo425@gmail.com>
2023-04-01 19:58:39 -0600

    added the tree algorithm

--- graphAlgorithm/makeGraph.py
+++ graphAlgorithm/makeGraph.py
@@ -1,65 +1,66 @@
 import pymongo
 import openai
+import networkx as nx
+import matplotlib.pyplot as plt
 from graphviz import Digraph
 
-def makegraph(userID):
+def makegraph(username):
     # Connect to the MongoDB database
     client = pymongo.MongoClient("mongodb+srv://user:HIZ05Jh0NVv6YKg@suplaisense.8zr73kh.mongodb.net/?retryWrites=true&w=majority")
     users_db = client.users
     invoice_db = client.invoices
 
     # Connect to the ChatGPT API
-    openai.api_key = "YOUR_API_KEY"
+    openai.api_key = "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"
     model_engine = "text-davinci-002"
-    product_keyword = "final product" # keyword to identify the final product being built
+    product_keyword = "any product" # keyword to identify the final product being built
 
     # Retrieve the customer's purchases
-    customer = users_db.users.find_one({'_id': userID})
-    customer_name = customer['customer_name']
-    customer_invoices = invoice_db.invoices.find({'user_id': customer['_id']})
+    customer = users_db.users.find_one({'customer_name': username})
+    customer_invoices = invoice_db.invoices.find({'buyer_name': username})
 
     # Analyze the product descriptions to determine which invoices are relevant
-    relevant_invoices = []
-    for invoice in customer_invoices:
-        product_descriptions = invoice['product_descriptions']
-        for product_description in product_descriptions:
+    connections=[[username,i['seller_name'],i['products'][:,0]] for i in customer_invoices]
+    relevant_invoices = [[i,"any product"] for i in customer_invoices]
+    while relevant_invoices:
+        invoice,product_target = relevant_invoices.pop(0)
+        for product_keyword,quantity in invoice['products']:
             response = openai.Completion.create(
                 engine=model_engine,
-                prompt=f"Is the following product relevant to the '{product_keyword}'?\n{product_description}",
+                prompt = f"Respond yes or no. Is {product_keyword} used in the production of {product_target} respond yes or no do not include an explanation'?\n",
                 max_tokens=50,
                 n=1,
                 stop=None,
                 temperature=0.5,
             )
             if response.choices[0].text.strip().lower() == "yes":
-                relevant_invoices.append(invoice)
+                for invoice in users_db.users.find_one({'customer_name': invoice['seller_name']}):
+                    relevant_invoices.append([invoice,product_keyword])
+                    connections.append([invoice['seller_name'],invoice['buyer_name'],product_keyword])
 
-    # Retrieve the suppliers for the relevant invoices
-    relevant_supplier_ids = set()
-    for invoice in relevant_invoices:
-        supplier_id = invoice['supplier_id']
-        relevant_supplier_ids.add(supplier_id)
-        while 'parent_supplier_id' in invoice:
-            parent_supplier_id = invoice['parent_supplier_id']
-            supplier = db.suppliers.find_one({'_id': parent_supplier_id})
-            if supplier:
-                relevant_supplier_ids.add(parent_supplier_id)
-            invoice = db.invoices.find_one({'_id': invoice['parent_invoice_id']})
 
-    relevant_suppliers = []
-    for supplier_id in relevant_supplier_ids:
-        supplier = db.suppliers.find_one({'_id': supplier_id})
-        if supplier:
-            relevant_suppliers.append(supplier)
+    # Create an empty graph
+    G = nx.Graph()
 
-    # Generate a flow chart diagram for the customer's relevant suppliers
-    dot = Digraph()
-    for supplier in relevant_suppliers:
-        dot.node(str(supplier['_id']), supplier['name'])
-        if 'parent_supplier_id' in supplier:
-            parent_supplier = db.suppliers.find_one({'_id': supplier['parent_supplier_id']})
-            dot.edge(str(parent_supplier['_id']), str(supplier['_id']))
+    # Add nodes to the graph for each supplier and buyer
+    for connection in connections:
+        supplier, buyer, _ = connection
+        G.add_node(supplier)
+        G.add_node(buyer)
 
-    dot.format = 'png'
-    dot.render(customer_name + '_relevant_suppliers', view=True)
+    # Add edges to the graph for each connection with the product information
+    for connection in connections:
+        supplier, buyer, product = connection
+        G.add_edge(supplier, buyer, product=str(product))
 
+    # Draw the graph with nodes labeled by their names and edges labeled by the product
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    edge_labels = nx.get_edge_attributes(G, 'product')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_weight='bold')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    makegraph('Super Home Builders')
